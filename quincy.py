155bbd349c1aec26edde1027bcbe3caa715b9630
Igor Prochazka <igor.prochazka@gmail.com>
2025-06-13 12:19:32 +0200

    Initial commit

---
+++ quincy.py
@@ -0,0 +1,22 @@
+from typing import Callable
+
+
+class Quincy:
+    def __init__(self, filename: str):
+        self.filename = filename
+        self._tasks: dict[str, Callable] = {}
+
+    def task(self, func):
+        """Decorator to register an async function as a task."""
+        self._tasks[func.__name__] = func
+
+        async def wrapper(*args, **kwargs):
+            return await self._tasks[func.__name__](*args, **kwargs)
+
+        # Store reference to this Quincy instance
+        wrapper._quincy = self
+        return wrapper
+
+    def run(self):
+        """Run the Quincy application."""
+        pass  # For now, just a placeholder
---
+++ worker.py
@@ -0,0 +1,17 @@
+import quincy
+
+app = quincy.Quincy(filename="quincy.db")
+
+
+@app.task
+async def hello_world():
+    print("Hello, world!")
+
+
+@app.task
+async def multiply(a: int, b: int) -> int:
+    return a * b
+
+
+if __name__ == "__main__":
+    app.run()
