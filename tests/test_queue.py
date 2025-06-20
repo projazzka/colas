c768bc4e2bb7d6c56373d3725a27d36c8719f8bd
Igor Prochazka <igor.prochazka@gmail.com>
2025-06-20 17:33:28 +0200

    Enhance test

--- tests/test_queue.py
+++ tests/test_queue.py
@@ -10,19 +10,26 @@ async def test_push_and_pop(temp_db_file):
     queue = Queue(str(temp_db_file), "test_queue")
     await queue.init()
 
-    task_id = uuid.uuid4()
-    name = "test_task"
-    args = [1, 2]
-    kwargs = {"c": 3}
+    task_id_1 = uuid.uuid4()
+    await queue.push(task_id_1, "test_task_1", *[1, 2], **{"a": 3})
 
-    await queue.push(task_id, name, *args, **kwargs)
+    task_id_2 = uuid.uuid4()
+    await queue.push(task_id_2, "test_task_2", *[4, 5], **{"b": 6})
 
-    popped_task = await queue.pop()
+    popped_task_1 = await queue.pop()
+    assert popped_task_1 is not None
+    popped_task_id_1, (popped_name_1, popped_args_1, popped_kwargs_1) = popped_task_1
+    assert popped_task_id_1 == task_id_1
+    assert popped_name_1 == "test_task_1"
+    assert popped_args_1 == [1, 2]
+    assert popped_kwargs_1 == {"a": 3}
 
-    assert popped_task is not None
-    popped_task_id, (popped_name, popped_args, popped_kwargs) = popped_task
+    popped_task_2 = await queue.pop()
+    assert popped_task_2 is not None
+    popped_task_id_2, (popped_name_2, popped_args_2, popped_kwargs_2) = popped_task_2
+    assert popped_task_id_2 == task_id_2
+    assert popped_name_2 == "test_task_2"
+    assert popped_args_2 == [4, 5]
+    assert popped_kwargs_2 == {"b": 6}
 
-    assert popped_task_id == task_id
-    assert popped_name == name
-    assert popped_args == args
-    assert popped_kwargs == kwargs
+    assert await queue.pop() is None
